3f3a6e7ca76d7e0138274b97a65748a181bb3123
99002605 <71481207+99002605@users.noreply.github.com>
2020-09-19 13:52:14 +0530

    Update test.c

--- imple/test/test.c
+++ imple/test/test.c
@@ -3,7 +3,7 @@
 
 /* Modify these two lines according to the project */
 #include <calculator_operations.h>
-#define PROJECT_NAME    "imple"
+#define PROJECT_NAME    "TEST"
 
 /* Prototypes for all the test functions */
 void test_addition(void);
